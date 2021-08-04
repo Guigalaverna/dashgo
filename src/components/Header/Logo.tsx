18184e90762dec28f835d6852303ca78f4f81465
Guigalaverna <guigalaverna@gmail.com>
2021-08-04 13:21:25 -0300

    Convert logo, search, notifications, avatar in component

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react";
+
+export function Logo() {
+  return (
+    <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+      dashgo
+      <Text as="span" fontSize="3xl" ml="1" color="purple.500">
+        .
+      </Text>
+    </Text>
+  );
+}
