995d3380a02c4386ee208c334d55fe8113300896
Guigalaverna <guigalaverna@gmail.com>
2021-08-04 13:21:15 -0300

    Convert logo, search, notifications, avatar in component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
-import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
-import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
+import { Flex } from "@chakra-ui/react";
+import { Logo } from "./Header/Logo";
+import { NotificationNav } from "./Header/NotificationNav";
+import { Profile } from "./Header/Profile";
+import { Search } from "./Header/Search";
 
 export function Header() {
   return (
@@ -13,63 +16,15 @@ export function Header() {
       align="center"
       px="6"
     >
-      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
-        dashgo
-        <Text as="span" fontSize="3xl" ml="1" color="purple.500">
-          .
-        </Text>
-      </Text>
-      <Flex
-        as="label"
-        flex="1"
-        py="4"
-        px="8"
-        ml="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        background="gray.800"
-        borderRadius="full"
-      >
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          px='4'
-          mr='4'
-          placeholder="Buscar na plataforma"
-          _placeholder={{ color: "gray.400" }}
-        />
-
-        <Icon as={RiSearchLine} fontSize='20' />
-      </Flex>
+      <Logo />
+      <Search />
       <Flex
         align='center'
         ml='auto'
       >
-        <HStack
-         spacing='4' 
-         mx='8'
-         pr='8'
-         py='1'
-         color='gray.300'
-         borderRightWidth={1}
-         borderColor='gray.700'
-        >
-          <Icon as={RiNotificationLine} fontSize='20' />
-          <Icon as={RiUserAddLine} fontSize='20' />
-        </HStack>
+        <NotificationNav />
 
-        <Flex align='center'>
-          <Box mr='4' textAlign='right'>
-            <Text>Guilherme Galaverna</Text>
-            <Text color='gray.300' fontSize='small'>
-              guigalaverna@gmail.com
-            </Text>
-          </Box>
-
-          <Avatar size='md' name='Guilherme Galaverna' src='https://github.com/Guigalaverna.png'/>
-        </Flex>
+       <Profile />
       </Flex>
     </Flex>
   );
