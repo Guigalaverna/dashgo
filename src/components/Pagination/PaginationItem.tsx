96ce2f41fdf132729b5504401985e25c9c78445b
Guigalaverna <guigalaverna@gmail.com>
2021-08-04 20:21:29 -0300

    Separate pagination into folder

---
+++ src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@chakra-ui/react";
+
+interface PaginationItemProps {
+  isCurrent?: boolean;
+  pageNumber: number;
+}
+
+export function PaginationItem({ isCurrent = false, pageNumber }: PaginationItemProps) {
+  if (isCurrent) {
+    return (
+      <Button
+        size="sm"
+        fontSize="xs"
+        w="4"
+        colorScheme="purple"
+        disabled
+        _disabled={{
+          bg: "purple.500",
+          cursor: "default",
+        }}
+      >
+        {pageNumber}
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      size="sm"
+      fontSize="xs"
+      w="4"
+      bg="gray.700"
+      _hover={{
+        bg: "gray.500",
+      }}
+    >
+      {pageNumber}
+    </Button>
+  );
+}
