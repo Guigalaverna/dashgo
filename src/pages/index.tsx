ceccd85ec67b85a8d0bff56ffd43e32ec2c75273
Guigalaverna <guigalaverna@gmail.com>
2021-08-04 03:31:41 -0300

    Create sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import {
+  Button,
+  Flex, Stack
+} from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+
 export default function Home() {
   return (
-    <h1>Hey</h1>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input name='email' label='E-mail' />
+          <Input name='password' label='Senha' />
+        </Stack>
+        <Button type="button" mt={6} colorScheme="purple" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
