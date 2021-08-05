2e95721e62fc9085fdba230fc8e03e3e3d5ef422
Guigalaverna <guigalaverna@gmail.com>
2021-08-05 01:54:20 -0300

    Remove adapting charts

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,11 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 
-
-const Chart = dynamic(() => import('react-apexcharts'), {
-  ssr: false
-})
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 const options = {
   chart: {
@@ -16,7 +15,7 @@ const options = {
     zoom: {
       enabled: false,
     },
-    foreColor: theme.colors.gray[500]
+    foreColor: theme.colors.gray[500],
   },
   grid: {
     show: false,
@@ -29,35 +28,33 @@ const options = {
   },
   xaxis: {
     axisBorder: {
-      color: theme.colors.gray[600]
+      color: theme.colors.gray[600],
     },
     axisTicks: {
-      color: theme.colors.gray[600]
+      color: theme.colors.gray[600],
     },
     categories: [
-      '2021-03-18T00:00:00.000Z',
-      '2021-03-19T00:00:00.000Z',
-      '2021-03-20T00:00:00.000Z',
-      '2021-03-21T00:00:00.000Z',
-      '2021-03-22T00:00:00.000Z',
-      '2021-03-23T00:00:00.000Z',
-      '2021-03-24T00:00:00.000Z'
-    ]
+      "2021-03-18T00:00:00.000Z",
+      "2021-03-19T00:00:00.000Z",
+      "2021-03-20T00:00:00.000Z",
+      "2021-03-21T00:00:00.000Z",
+      "2021-03-22T00:00:00.000Z",
+      "2021-03-23T00:00:00.000Z",
+      "2021-03-24T00:00:00.000Z",
+    ],
   },
   fill: {
     opacity: 0.3,
-    type: 'gradient',
+    type: "gradient",
     gradient: {
-      shade: 'dark',
+      shade: "dark",
       opacityFrom: 0.7,
-      opacityTo: 0.3
-    }
-  }
-}
+      opacityTo: 0.3,
+    },
+  },
+};
 
-const series = [
-  { name: 'Series 1', data: [31, 442, 132, 866, 132, 442, 132] }
-]
+const series = [{ name: "Series 1", data: [31, 442, 132, 866, 132, 442, 132] }];
 
 export default function Dashboard() {
   return (
@@ -68,29 +65,39 @@ export default function Dashboard() {
         <Sidebar />
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius="8">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius="8">
             <Text fontSize="lg" mb="4">
               Inscritos da Semana
             </Text>
-            <Chart type='area' height={168} options={{
-              ...options,
-              xaxis: {
-                ...options.xaxis,
-                type: 'datetime'
-              }
-            }} series={series} />
+            <Chart
+              type="area"
+              height={168}
+              options={{
+                ...options,
+                xaxis: {
+                  ...options.xaxis,
+                  type: "datetime",
+                },
+              }}
+              series={series}
+            />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius="8">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius="8">
             <Text fontSize="lg" mb="4">
               Taxa de Abertura
             </Text>
-            <Chart type='area' height={168} options={{
-              ...options,
-              xaxis: {
-                ...options.xaxis,
-                type: 'datetime'
-              }
-            }} series={series} />
+            <Chart
+              type="area"
+              height={168}
+              options={{
+                ...options,
+                xaxis: {
+                  ...options.xaxis,
+                  type: "datetime",
+                },
+              }}
+              series={series}
+            />
           </Box>
         </SimpleGrid>
       </Flex>
