5e240a11d52374dcd9c094ddfe612e915ebcb07e
Guigalaverna <guigalaverna@gmail.com>
2021-08-13 03:15:10 -0300

    setup react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,26 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { AppProps } from 'next/app'
-import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
-import { makeServer } from '../services/mirage'
-import { theme } from '../styles/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
+import { makeServer } from "../services/mirage";
+import { theme } from "../styles/theme";
 
-if (process.env.NODE_ENV === 'development') {
-  makeServer()
+if (process.env.NODE_ENV === "development") {
+  makeServer();
 }
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <SidebarDrawerProvider>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </SidebarDrawerProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
