01ef35113317f0af3976dfa4d56261cdc8bbe963
Guigalaverna <guigalaverna@gmail.com>
2021-08-13 03:15:31 -0300

    show table if have data in cache

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -17,11 +18,19 @@ import {
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -30,7 +39,7 @@ export default function UserList() {
   useEffect(() => {
     fetch("http://localhost:3000/api/users")
       .then((response) => response.json())
-      .then(data => console.log(data))
+      .then((data) => console.log(data));
   }, []);
 
   return (
@@ -58,65 +67,77 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                  <Checkbox colorScheme="purple" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="purple" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Guilherme Galaverna</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      guigalaverna@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de abril, 2021</Td>}
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="purple" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Guilherme Galaverna</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      guigalaverna@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de abril, 2021</Td>}
-                <Td></Td>
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="purple" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Guilherme Galaverna</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      guigalaverna@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de abril, 2021</Td>}
-                <Td></Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="purple" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="purple" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Guilherme Galaverna</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          guigalaverna@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de abril, 2021</Td>}
+                    <Td></Td>
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="purple" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Guilherme Galaverna</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          guigalaverna@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de abril, 2021</Td>}
+                    <Td></Td>
+                  </Tr>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="purple" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Guilherme Galaverna</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          guigalaverna@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>04 de abril, 2021</Td>}
+                    <Td></Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
